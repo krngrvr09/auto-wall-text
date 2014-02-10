7476276ac3c4d9135950f62cd65df8b71d4dfc93
Karan <karan@karan-HP-Pavilion-dm4-Notebook-PC.(none)>
2014-02-10 19:51:08 +0530

    Able to track what the user is typing, and printing them back on the screen.

---
+++ js/count.js
@@ -0,0 +1,13 @@
+var ignore= new Array();
+
+function customcount(ignore)
+{
+	var filecont= document.getElementById('input').value;
+	
+	document.getElementById('head').innerHTML=filecont
+	
+	
+	
+}
+
+
