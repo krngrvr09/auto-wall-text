69418d951d0c1f67e33c4dbb5ff299aa6d7cd538
Karan <karan@karan-HP-Pavilion-dm4-Notebook-PC.(none)>
2014-02-11 07:41:44 +0530

    Able to track word by word, using 'space' as a delimiter, what user is typing, in real time.

--- js/count.js
+++ js/count.js
@@ -1,13 +1,38 @@
-var ignore= new Array();
+//var ignore= new Array();
 
-function customcount(ignore)
+var temp=0;
+var temp1=0;
+
+$(document).keydown(function(e)
+{
+	if (e.keyCode==32)
+	{	customcount();
+		//temp=temp+1;
+		temp1=temp
+		//temp=temp-2;
+		return false;
+	}
+	if (e.keycode!=32)
+	{
+		temp=temp+1;
+	}
+
+});
+
+
+function customcount()
 {
 	var filecont= document.getElementById('input').value;
+	//document.write('jhsgdjshdgsj');
+	//var temp=0;
+	//temp=filecont.indexOf(' ');
+	//x=filecont.indexOf(' ',temp);
+	filecont=filecont.slice(temp1,5000);
 	
-	document.getElementById('head').innerHTML=filecont
+	document.getElementById('head').innerHTML=temp1;
+	document.getElementById('head1').innerHTML=filecont;
 	
-	
+	//flag=1;
 	
 }
-
 
