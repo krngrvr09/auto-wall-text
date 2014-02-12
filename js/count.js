cd111af1b24453ed488cf86d4c7f8f87db410753
Karan <karan@karan-HP-Pavilion-dm4-Notebook-PC.(none)>
2014-02-13 04:56:11 +0530

    Basic implementation is complete. Looking good now.

--- js/count.js
+++ js/count.js
@@ -3,10 +3,11 @@ var innerarr= new Array();
 var temp=0;
 var temp1=0;
 var ignore=["I", "me", "my", "mine", "we", "us", "our", "ours", "you", "your", "yours", "he", "him", "his", "she", "her", "hers", "it", "its", "they", "their", "theirs", "she", "her", "hers", "he", "him", "his", "it", "its", "they", "them", "their", "theirs", "this", "that", "these", "those", "theirselves", "theirself", "hisself", "ourself", "someone", "somebody", "something", "anyone", "anybody", "anything", "no", "one", "nobody", "nothing", "everyone", "everybody", "everything", "both", "few", "several", "some", "any", "none", "all", "most", "who", "whose", "whom", "which", "what", "a", "an", "the", "though", "although", "eventhough", "while", "if", "only", "if", "unless", "until", "provided", "that", "assuming", "that", "even", "if", "in", "case", "test", "than", "rather", "than", "whether", "as", "much", "as", "where", "as", "after", "as", "long", "as", "as", "soon", "as", "before", "by", "the", "time", "now", "that", "once", "since", "till", "until", "when", "whenever", "while", "because", "since", "so", "that", "in", "order", "why", "how", "as", "though", "as", "if", "and","is",".",",","<",">","/","?",";",":","'","[","]","{","}","-", "=","_","+",")","(","*","&","^","%","$","#","@","!"]
-$(document).keydown(function(e)
+$(document).keyup(function(e)
 {
 	if (e.keyCode==32 || e.keyCode==13)
-	{	document.getElementById('input').innerHTML=document.getElementById('input').value+" ";
+	{	temp=temp+1;
+		//document.getElementById('input').innerHTML=document.getElementById('input').value+" ";
 		customcount();
 		
 		//temp=temp+1;
@@ -28,8 +29,8 @@ function customcount()
 	//temp=filecont.indexOf(' ');
 	//x=filecont.indexOf(' ',temp);
 	filecont=filecont.slice(temp1,5000);	
-	if (ignore.indexOf(filecont)==-1)
-	{
+	//if (ignore.indexOf(filecont)==-1)
+	//{
 	var h1=document.createElement("H1");
 	var t=document.createTextNode(filecont);
 	h1.appendChild(t);
@@ -40,7 +41,7 @@ function customcount()
 	//$(document).ready(function(){
 	//$("head").fadeIn();
   	//});
-	}
+	//}
 	var top=Math.floor((Math.random()*400)+1);
 	var left=Math.floor((Math.random()*700)+1);
 	//document.getElementById('head').style.position="absolute";
--- readme.md
+++ readme.md
@@ -1,7 +1,1 @@
-The idea is to, generate a wallpaper for you in real time as you type your text.
-
-Right now, I will be working on, using words, except for the conjunctions, articles etc., to be added in your background.
-
-After that May be we can, match pics, according to the text you type, and then put up that as your background.
-
-Lets see.
+The beasic idea behind this project, is to, flash words, as you type them in your backgound. This happens in a vary sen-ish and minimalistic way. Please clone, in order to experience it your self. If you'd like to contribute, please, fork.
